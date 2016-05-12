a7e73570871e076f1b6aafd24b075f06f0e4ee84
gsduong <nguyenduong.ict.hust@gmail.com>
2016-05-12 10:58:15 +0700

    Add image to category

--- app/Category.php
+++ app/Category.php
@@ -9,7 +9,7 @@ class Category extends Model
     //
     protected $table = 'categories';
 
-    protected $fillable = ['name', 'name_vi', 'name_en', 'description'];
+    protected $fillable = ['name', 'name_vi', 'name_en', 'description', 'image'];
 
     public function products(){
     	return $this->belongsToMany('App\Products', 'category_product', 'category_id', 'product_id')->withPivot('product_id');
