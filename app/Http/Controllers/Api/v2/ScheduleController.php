efa2fbccda2aa879c27d38ca1ffce40c714f58c3
gsduong <nguyenduong.ict.hust@gmail.com>
2016-05-22 21:13:41 +0700

    Fix schedulecontroller syntax

--- app/Http/Controllers/Api/v2/ScheduleController.php
+++ app/Http/Controllers/Api/v2/ScheduleController.php
@@ -51,7 +51,6 @@ class ScheduleController extends Controller
             $item_type = $this->item_type($schedule->start_time, $schedule->end_time, $utc_current_time);
             $item = ['id' => $schedule->product_id, 'title' => $product->title, 'channel_id' => $product->channel_id, 'image_link' => $product->image_link, 'video_link' => $product->video_link, 'product_link' => $product->product_link, 'description' => $product->description, 'old_price' => $product->old_price, 'new_price' => $product->new_price, 'start_time' => $schedule->start_time, 'end_time' => $schedule->end_time, 'stream_link' => $schedule->stream_link, 'item_type' => $item_type];
             if ($item_type != -1) {
-                var_dump($item);
                 array_push($array, $item);
             }
         }
